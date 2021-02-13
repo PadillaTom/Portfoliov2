921bc9485463312db11182ab1949c32f80e26d55
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-13 08:40:22 +0100

    Cmabiando Idea, Probar Errores en HTML Version

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className='navbar-container'>
       <div className='navbar-center'>
         <div className='nav-left'>
           <Link to='/'>Tomas Padilla</Link>
         </div>
-        <div className='nav-right'>
+        <div
+          className={isOpen ? 'nav-right show-close' : 'nav-right'}
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <div className='burger-line'></div>
           <div className='burger-line'></div>
           <div className='burger-line'></div>
