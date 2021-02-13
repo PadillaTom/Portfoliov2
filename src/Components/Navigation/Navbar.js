0873eaca4b1d435e719b4d8530b14f44786a9f49
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-13 11:16:55 +0100

    Comenzamos Todo

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { AiOutlineBars, AiOutlineClose } from 'react-icons/ai';
+import { Sidebar } from '../Navigation';
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className='navbar-container'>
-      <div className='navbar-center'>
-        <div className='nav-left'>
-          <Link to='/'>Tomas Padilla</Link>
+    <>
+      <nav className='navbar-container'>
+        <div className='navbar-center'>
+          <div className='nav-left'>
+            <Link to='/'>Tomas Padilla</Link>
+          </div>
+          <div className='nav-right' onClick={() => setIsOpen(!isOpen)}>
+            {/* CAMBIAR POR SVG EN UN FUTURO */}
+            {isOpen ? (
+              <AiOutlineClose className='fa-times'></AiOutlineClose>
+            ) : (
+              <AiOutlineBars className='fa-bars'></AiOutlineBars>
+            )}
+            {/* END CAMBIAR POR SVG EN UN FUTURO */}
+          </div>
         </div>
-        <div
-          className={isOpen ? 'nav-right show-close' : 'nav-right'}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <div className='burger-line'></div>
-          <div className='burger-line'></div>
-          <div className='burger-line'></div>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      {isOpen && <Sidebar></Sidebar>}
+    </>
   );
 };
 
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { homepage } from '../Utils/data';
+import heroBg from '../Assets/Images/heroBg.jpg';
 
 const Homepage = () => {
   const [data, setData] = useState([]);
@@ -13,15 +14,21 @@ const Homepage = () => {
   return (
     <>
       <section className='home-sect'>
+        <div className='home-bg'>
+          <img src={heroBg} alt='Tomas Padilla Homepage' />
+          <div className='home-bg-overlay'></div>
+          <div className='home-bg-circle'></div>
+        </div>
         <div className='homepage-container'>
           <h6>Tomas Padilla</h6>
-          <h1>Web Developer</h1>
-          <Link to='/works'>
-            <button type='button' className='homeCTA'>
-              Works
-            </button>
-          </Link>
+          <h2>Web Developer</h2>
         </div>
+        <Link to='/works' className='homeCTA'>
+          <button type='button'>
+            <span className='span1'>Works</span>
+            <span className='span2'>Explore</span>
+          </button>
+        </Link>
       </section>
     </>
   );
