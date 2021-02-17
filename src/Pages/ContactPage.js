19aa7b352c442b3127eda959e54465bcb590241d
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-17 15:07:48 +0100

    WOrks Queries and DONE

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -12,55 +12,64 @@ const ContactPage = () => {
 
   return (
     <section className='section contact-sect'>
+      {/* IMG Mobile */}
       <div className='contact-img-container'>
         <img src={contactImg} alt='Contact Tomas Padilla' />
       </div>
+
       <div className='contact-data-container'>
-        <div className='contact-data'>
-          <h4>Get in Touch</h4>
-          <a href='mailto:padillatomasagustin@gmail.com'>
-            padillatomasagustin@gmail.com
-          </a>
-          <br />
-          <a href='tel:+41794002693'>+41 79 400 26 93</a>
-        </div>
-        <div className='contact-data cont-social'>
-          <h4>Social Media</h4>
-          <a
-            href='https://github.com/PadillaTom'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <AiFillGithub></AiFillGithub>
-          </a>
-          <a
-            href='https://www.linkedin.com/in/padillatom/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <AiFillLinkedin></AiFillLinkedin>
-          </a>
+        <div className='screen-data-center'>
+          <div className='contact-data'>
+            <h4>Get in Touch</h4>
+            <a href='mailto:padillatomasagustin@gmail.com'>
+              padillatomasagustin@gmail.com
+            </a>
+            <br />
+            <a href='tel:+41794002693'>+41 79 400 26 93</a>
+          </div>
+          <div className='contact-data cont-social'>
+            <h4>Social Media</h4>
+            <a
+              href='https://github.com/PadillaTom'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <AiFillGithub></AiFillGithub>
+            </a>
+            <a
+              href='https://www.linkedin.com/in/padillatom/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <AiFillLinkedin></AiFillLinkedin>
+            </a>
+          </div>
         </div>
       </div>
+      <div className='contact-img-container-screens'>
+        <img src={contactImg} alt='Contact Tomas Padilla' />
+      </div>
       <div className='contact-form-container'>
-        <h2>Send Message</h2>
-        <form>
-          <div className='single-control'>
-            <label htmlFor='name'>Your Name</label>
-            <input type='text' />
-          </div>
-          <div className='single-control'>
-            <label htmlFor='email'>Your E-mail</label>
-            <input type='email' />
-          </div>
-          <div className='single-control'>
-            <label htmlFor='message' className='textarea-label'>
-              Your Message
-            </label>
-            <textarea name='message' id='' cols='30' rows='10'></textarea>
-          </div>
-          <button type='submit'>Send Message</button>
-        </form>
+        <div className='screen-form-center'>
+          <h2>Send Message</h2>
+          <form>
+            <div className='single-control'>
+              <label htmlFor='name'>Your Name</label>
+              <input type='text' />
+            </div>
+            <div className='single-control'>
+              <label htmlFor='email'>Your E-mail</label>
+              <input type='email' />
+            </div>
+            <div className='single-control'>
+              <label htmlFor='message' className='textarea-label'>
+                Your Message
+              </label>
+              <textarea name='message' id='' cols='30' rows='10'></textarea>
+            </div>
+            <button type='submit'>Send Message</button>
+          </form>
+        </div>
       </div>
     </section>
   );
