f1ece9201430a3537844af8ae92b77189cc8fd4e
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-17 09:10:23 +0100

    Adding some Works

--- src/Pages/WorksPage.js
+++ src/Pages/WorksPage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 
 import { useNavigationContext } from '../Context/navigation_context';
 import { Footer } from '../Components/Navigation';
+import project1 from '../Assets/Images/project1.jpg';
+import project2 from '../Assets/Images/project2.jpg';
 
 const WorksPage = () => {
   const { setColorLight } = useNavigationContext();
@@ -10,10 +12,41 @@ const WorksPage = () => {
   }, []);
 
   return (
-    <div>
-      My works: Page
+    <section className='section works-sect'>
+      <div className='works-title'>
+        <h1>Works</h1>
+        <div className='works-underline'></div>
+      </div>
+
+      {/* Pongo Filter? */}
+      <div className='single-works-container'>
+        {/* Kampai Boutique */}
+        <div className='single-work-1'>
+          <div className='work-img-container'>
+            <img src={project1} alt='Kampai Boutique Tomas Padilla' />
+          </div>
+          <div className='single-work-title'>
+            <h4>Kampai Boutique</h4>
+            <div className='single-work-cta'>Details</div>
+          </div>
+        </div>
+
+        {/* Fincas Manolo */}
+        <div className='single-work-2'>
+          <div className='work-img-container'>
+            <img src={project2} alt='Fincas Manolo Tomas Padilla' />
+          </div>
+          <div className='single-work-title'>
+            <h4>Fincas Manolo</h4>
+            <div className='single-work-cta'>Details</div>
+          </div>
+        </div>
+
+        {/* Project 3 */}
+      </div>
+
       <Footer></Footer>
-    </div>
+    </section>
   );
 };
 
