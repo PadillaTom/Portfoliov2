d79c729d10044129ee42a841fa7f3bdcbb847926
Tomas Padilla <padillatomasagustin@gmail.com>
2022-01-04 11:22:17 +0100

    Avanzando  mi CV

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,130 +1,152 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import {FaAngleDoubleDown} from "react-icons/fa";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaAngleDoubleDown } from "react-icons/fa";
 
-import aboutImg from '../Assets/Images/aboutImg.jpg';
-import { useNavigationContext } from '../Context/navigation_context';
+import aboutImg from "../Assets/Images/aboutImg.jpg";
+import { useNavigationContext } from "../Context/navigation_context";
 // import { Reviews } from '../Components/Reviews';
-import { Footer } from '../Components/Navigation';
+import { Footer } from "../Components/Navigation";
 
 // Animations:
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import {
-  pageVars,
-  pageTrans,
-  varsAboutImgBg,
-  varsContImg,
-  varsAboutScroll,
-} from '../Utils/helpers';
+	pageVars,
+	pageTrans,
+	varsAboutImgBg,
+	varsContImg,
+	varsAboutScroll,
+} from "../Utils/helpers";
 
 const AboutPage = () => {
-  // Context
-  const { setColorLight } = useNavigationContext();
-  useEffect(() => {
-    setColorLight();    
-  }, []);
+	// Context
+	const { setColorLight } = useNavigationContext();
+	useEffect(() => {
+		setColorLight();
+	}, []);
 
-  return (
-    <>
-      <motion.section
-        initial='out'
-        animate='in'
-        exit='out'
-        key='About'
-        variants={pageVars}
-        transition={pageTrans}
-        className='section about-sect'
-      >
-        {/* Title */}
-        <div className='about-title'>
-          <h6>About Me</h6>
-          <h2>Tomas Padilla</h2>
-        </div>
-        {/* Image */}
-        <div className='aboutImg-container'>
-          <motion.img
-            initial='from'
-            animate='to'
-            variants={varsContImg}
-            transition={{ duration: 0.7, ease: 'easeIn' }}
-            key='caboutimgmobile'
-            src={aboutImg}
-            alt='About Tomas Padilla'
-          />
-        </div>
+	return (
+		<>
+			<motion.section
+				initial="out"
+				animate="in"
+				exit="out"
+				key="About"
+				variants={pageVars}
+				transition={pageTrans}
+				className="section about-sect"
+			>
+				{/* Title */}
+				<div className="about-title">
+					<h6>About Me</h6>
+					<h2>Tomas Padilla</h2>
+				</div>
+				{/* Image */}
+				<div className="aboutImg-container">
+					<motion.img
+						initial="from"
+						animate="to"
+						variants={varsContImg}
+						transition={{ duration: 0.7, ease: "easeIn" }}
+						key="caboutimgmobile"
+						src={aboutImg}
+						alt="About Tomas Padilla"
+					/>
+				</div>
 
-        {/* WideScreen */}
-        <div className='about-widescreen-container'>
-          {/* Title */}
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.8, duration: 0.4, ease: 'easeIn' }}
-            key='aboutTitle'
-            className='about-title-w'
-          >
-            <div className='aboutscreens-title'>
-              <h6>About Me</h6>
-              <h2>Tomas Padilla</h2>
-            </div>
-            <motion.div 
-              initial="from"
-              animate="to"
-              variants={varsAboutScroll}
-              transition={{ease: "easeIn", duration: 0.4}}
-              className='about-scroll-down'>              
-              <FaAngleDoubleDown></FaAngleDoubleDown>
-            </motion.div>
-          </motion.div>
-          {/* Image */}
-          <motion.div
-            initial='from'
-            animate='to'
-            variants={varsAboutImgBg}
-            transition={{ ease: 'easeIn', duration: 0.6 }}
-            key='AboutimgContainer'
-            className='aboutImg-container-w'
-          >
-            <div className='about-img-center'>
-              <motion.img
-                initial={{ opacity: 0, y: '2rem' }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 1, duration: 0.9, ease: "easeInOut" }}
-                key='aboutImgDelayed'
-                src={aboutImg}
-                alt='About Tomas Padilla'
-              />
-            </div>
-          </motion.div>
-        </div>
-        {/* End Widescreen */}
+				{/* WideScreen */}
+				<div className="about-widescreen-container">
+					{/* Title */}
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ delay: 0.8, duration: 0.4, ease: "easeIn" }}
+						key="aboutTitle"
+						className="about-title-w"
+					>
+						<div className="aboutscreens-title">
+							<h6>About Me</h6>
+							<h2>Tomas Padilla</h2>
+						</div>
+						<motion.div
+							initial="from"
+							animate="to"
+							variants={varsAboutScroll}
+							transition={{ ease: "easeIn", duration: 0.4 }}
+							className="about-scroll-down"
+						>
+							<FaAngleDoubleDown></FaAngleDoubleDown>
+						</motion.div>
+					</motion.div>
+					{/* Image */}
+					<motion.div
+						initial="from"
+						animate="to"
+						variants={varsAboutImgBg}
+						transition={{ ease: "easeIn", duration: 0.6 }}
+						key="AboutimgContainer"
+						className="aboutImg-container-w"
+					>
+						<div className="about-img-center">
+							<motion.img
+								initial={{ opacity: 0, y: "2rem" }}
+								animate={{ opacity: 1, y: 0 }}
+								transition={{ delay: 1, duration: 0.9, ease: "easeInOut" }}
+								key="aboutImgDelayed"
+								src={aboutImg}
+								alt="About Tomas Padilla"
+							/>
+						</div>
+					</motion.div>
+				</div>
+				{/* End Widescreen */}
 
-        {/* Text */}
-        <div className='division-line'></div>
-        <div className='about-text'>
-          With an artistic background, I have started my studies as a Web
-          Developer to find a real passion.
-          <br />
-          <br />
-          <span>Tools & Technologies:</span>
-          <br />
-          HTML, CSS, SASS, Javascript, React, Material UI, Styled Components, GSAP, Framer Motion.
-          <br/>
-          <span>Currently working on: </span>
-          <br/>
-          React Native, Java, Spring Boot, MySQL, MongoDB.
-          <br />
-          <br />I invite you to see my {" "}
-          <Link to='/works' className='aboutCTA'>
-            Previous Works
-          </Link>
-          .
-        </div>
-        {/* <Reviews></Reviews> */}
-        <Footer></Footer>
-      </motion.section>
-    </>
-  );
+				{/* Text */}
+				<div className="division-line"></div>
+				<div className="about-text">
+					With an artistic background, I have started my studies as a Web
+					Developer to find a real passion.
+					<br />
+					<br />
+					<span>Tools & Technologies:</span>
+					<br />
+					HTML, CSS, SASS, Javascript, React, Material UI, Styled Components,
+					GSAP, Framer Motion.
+					<br />
+					<span style={{ height: "0.9rem", display: "block" }}></span>
+					Java EE, Spring Framework, Spring Boot, JUnit, Swagger, PostgreSQL,
+					MySQL.
+					<br />
+					<span style={{ height: "0.9rem", display: "block" }}></span>
+					Jira, SourceTree, Postman.
+					<br />
+					<br />
+					<span>Currently working on: </span>
+					<br />
+					Spring Security, Spring Cloud, Oracle Cloud Infrastructure
+					<br />
+					<br />
+					<span>Objectives: </span>
+					<br />
+					As a developer, I would love to work with Microservices / Cloud Native
+					applications.
+					<br />
+					<span style={{ height: "3rem", display: "block" }}></span>
+					<div
+						className="division-line"
+						style={{ marginTop: "0rem", marginBottom: "3rem" }}
+					></div>
+					I invite you to see my
+					<Link to="/works" className="aboutCTA">
+						{" "}
+						Previous Works
+					</Link>
+					.
+				</div>
+				{/* <Reviews></Reviews> */}
+				<Footer></Footer>
+			</motion.section>
+		</>
+	);
 };
 
 export default AboutPage;
--- src/Utils/data.js
+++ src/Utils/data.js
@@ -8,86 +8,106 @@ import { AiOutlineGithub } from "react-icons/ai";
 import kb from "../Assets/Images/Projects/KampaiBoutique.jpg";
 import tp from "../Assets/Images/Projects/TPFinal.png";
 import artStore from "../Assets/Images/Projects/ArtStoreProject.png";
+import fundacionArg from "../Assets/Images/Projects/FundacionArg.png";
 
 export const workDetails = [
-  {
-    id: 1,
-    right: true,
-    slug: "kampai-boutique",
-    title: "Kampai Boutique",
-    workImg: kb,
-    client: "Kampai Boutique",
-    location: "Geneva, Switzerland",
-    launch: "2021",
-    live: "https://kampaiboutique.ch/",
-    liveInsta: "https://www.instagram.com/kampaiboutique/",
-    code: null,
-    desc: `Website and Instagram Campaign for a well-kown Interior Designer and
+	{
+		id: 1,
+		right: true,
+		slug: "kampai-boutique",
+		title: "Kampai Boutique",
+		workImg: kb,
+		client: "Kampai Boutique",
+		location: "Geneva, Switzerland",
+		launch: "2021",
+		live: "https://kampaiboutique.ch/",
+		liveInsta: "https://www.instagram.com/kampaiboutique/",
+		code: null,
+		desc: `Website and Instagram Campaign for a well-kown Interior Designer and
             Architect. Located in Geneva the goal is to showcase previous
             works and to dynamically change the Collections as needed.`,
-    stack: `Wordpress | Avada Builder | Photoshop | Illustrator | InDesign | Facebook Business Manager`,
-    nextWork: "TP Final Fullstack Java",
-    nextWorkUrl: `/works/tpfinal-polotic`,
-    shortStack: "Wordpress",
-    moreLinks: [],
-  },
-  {
-    id: 2,
-    right: false,
-    slug: "tpfinal-polotic",
-    title: "TP Final",
-    workImg: tp,
-    client: "Polotic's Final Project",
-    location: "Buenos Aires, Argentina",
-    launch: "2021",
-    live: "https://tpfinal-polotic.netlify.app/",
-    code: null,
-    desc: `Final Project for Polotic's "Desarrollo Web Fullstack Con Java". Originally made with JSP-Servlet, then migrated to Spring Boot - React to be deployed. 
+		stack: `Wordpress | Avada Builder | Photoshop | Illustrator | InDesign | Facebook Business Manager`,
+		nextWork: "TP Final Fullstack Java",
+		nextWorkUrl: `/works/tpfinal-polotic`,
+		shortStack: "Wordpress",
+		moreLinks: [],
+	},
+	{
+		id: 2,
+		right: false,
+		slug: "tpfinal-polotic",
+		title: "TP Final",
+		workImg: tp,
+		client: "Polotic's Final Project",
+		location: "Buenos Aires, Argentina",
+		launch: "2021",
+		live: "https://tpfinal-polotic.netlify.app/",
+		code: null,
+		desc: `Final Project for Polotic's "Desarrollo Web Fullstack Con Java". Originally made with JSP-Servlet, then migrated to Spring Boot - React to be deployed. 
     It is a CRUD Application to manage Employees, Reservations and Hosts. Calculate Monthly and Daily earnings, Reservations made by Employees, List of Hosts, Display Room Information, and much more.`,
-    stack: `React | Context API | SASS | RESTful Services | Spring Boot | Data JPA | MySQL`,
-    nextWork: "Art And Store",
-    nextWorkUrl: `/works/art-store`,
-    shortStack: "Fullstack Java",
-    moreLinks: [
-      {
-        id: 1,
-        name: "JSP-Servlet",
-        url: "https://github.com/PadillaTom/TPFinalTomasPadilla",
-        icon: <AiOutlineGithub></AiOutlineGithub>,
-      },
-      {
-        id: 2,
-        name: "React",
-        url: "https://github.com/PadillaTom/SpringBoot-Polotic-TPFinal-Frontend",
-        icon: <AiOutlineGithub></AiOutlineGithub>,
-      },
-      {
-        id: 3,
-        name: "Spring Boot",
-        url: "https://github.com/PadillaTom/SpringBoot-Polotic-TPFinal",
-        icon: <AiOutlineGithub></AiOutlineGithub>,
-      },
-    ],
-  },
-  {
-    id: 3,
-    right: true,
-    slug: "art-store",
-    title: "Art And Store",
-    workImg: artStore,
-    client: "Practice Project",
-    location: "Geneva, Switzerland",
-    launch: "2021",
-    live: "https://artandstore.netlify.app/",
-    code: "https://github.com/PadillaTom/Art-Store-ReactJS",
-    desc: `Practice project for a Fictional Boutique. 100% Responsive using Styled Components and Media Queries. Users may browse and view Products through a variety of Filters, Add products to Cart and complete checkout with Stripe Gateway. Managing the Data with Context API, I am able to reduce the number of API Requests and avoid "Prop Drilling". `,
-    stack:
-      "React | John Smilga's Products API | AXIOS | Context API | Styled Components | Framer-Motion | React-Intersection-Observer | Auth0 | Stripe",
-    nextWork: "Kampai Boutique",
-    nextWorkUrl: `/works/kampai-boutique`,
-    shortStack: "ReactJS",
-    moreLinks: [],
-  },
+		stack: `React | Context API | SASS | RESTful Services | Spring Boot | Data JPA | MySQL`,
+		nextWork: "Art And Store",
+		nextWorkUrl: `/works/art-store`,
+		shortStack: "Fullstack Java",
+		moreLinks: [
+			{
+				id: 1,
+				name: "JSP-Servlet",
+				url: "https://github.com/PadillaTom/TPFinalTomasPadilla",
+				icon: <AiOutlineGithub></AiOutlineGithub>,
+			},
+			{
+				id: 2,
+				name: "React",
+				url: "https://github.com/PadillaTom/SpringBoot-Polotic-TPFinal-Frontend",
+				icon: <AiOutlineGithub></AiOutlineGithub>,
+			},
+			{
+				id: 3,
+				name: "Spring Boot",
+				url: "https://github.com/PadillaTom/SpringBoot-Polotic-TPFinal",
+				icon: <AiOutlineGithub></AiOutlineGithub>,
+			},
+		],
+	},
+	{
+		id: 3,
+		right: true,
+		slug: "art-store",
+		title: "Art And Store",
+		workImg: artStore,
+		client: "Practice Project",
+		location: "Geneva, Switzerland",
+		launch: "2021",
+		live: "https://artandstore.netlify.app/",
+		code: "https://github.com/PadillaTom/Art-Store-ReactJS",
+		desc: `Practice project for a Fictional Boutique. 100% Responsive using Styled Components and Media Queries. Users may browse and view Products through a variety of Filters, Add products to Cart and complete checkout with Stripe Gateway. Managing the Data with Context API, I am able to reduce the number of API Requests and avoid "Prop Drilling". `,
+		stack:
+			"React | John Smilga's Products API | AXIOS | Context API | Styled Components | Framer-Motion | React-Intersection-Observer | Auth0 | Stripe",
+		nextWork: "Fundacion Arg",
+		nextWorkUrl: `/works/fundacion-arg`,
+		shortStack: "ReactJS",
+		moreLinks: [],
+	},
+	{
+		id: 4,
+		right: false,
+		slug: "fundacion-arg",
+		title: "Fundacion Argentina",
+		workImg: fundacionArg,
+		client: "Fundacion Somos Mas",
+		location: "Salta, Argentina",
+		launch: "2022",
+		live: "https://alkemy-ot111.herokuapp.com/api/docs/swagger-ui/index.html?configUrl=/api/docs/swagger-config",
+		code: "https://github.com/PadillaTom/SpringBoot-FundacionArg",
+		desc: `Applying Agile Practices (SCRUM): Developed a REST API - Secured Endpoints and handled Authorization with Spring Security - Integrated AWS S3 / SendGrid - Testing with JUnit and Mockito - Documentation with Swagger`,
+		stack:
+			"Spring Boot | PostgreSQL | Gradle | Spring Security | Swagger | JUnit | AWS S3 | SendGrid",
+		nextWork: "Kampai Boutique",
+		nextWorkUrl: `/works/kampai-boutique`,
+		shortStack: "Spring Boot",
+		moreLinks: [],
+	},
 ];
 
 /*
@@ -97,24 +117,24 @@ export const workDetails = [
 */
 
 export const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 3,
-    title: "Works",
-    url: "/works",
-  },
-  {
-    id: 4,
-    title: "Contact",
-    url: "/contact",
-  },
+	{
+		id: 1,
+		title: "Home",
+		url: "/",
+	},
+	{
+		id: 2,
+		title: "About",
+		url: "/about",
+	},
+	{
+		id: 3,
+		title: "Works",
+		url: "/works",
+	},
+	{
+		id: 4,
+		title: "Contact",
+		url: "/contact",
+	},
 ];
